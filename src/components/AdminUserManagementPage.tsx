b42e094243b8bf29bf33aafcd09caf08da5cfd20
Yuvasubharama2 <yuvasubharama2@gmail.com>
2025-07-06 15:53:39 +0530

    Updated AdminUserManagementPage.tsx

--- src/components/AdminUserManagementPage.tsx
+++ src/components/AdminUserManagementPage.tsx
@@ -860,7 +860,6 @@ export default function AdminUserManagementPage() {
                     />
                   </div>
                 )}
-              </div>
               
               <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
                 <button
